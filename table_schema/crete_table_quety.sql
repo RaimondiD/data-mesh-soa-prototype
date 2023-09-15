9e60c81d95525a0d8144ca498da177101147ef82
Davide Raimondi <daduraimondi@gmail.com>
2023-09-15 11:45:11 +0200

    schema of sql table for testing

--- table_schema/crete_table_quety.sql
+++ table_schema/crete_table_quety.sql
@@ -1,13 +1,13 @@
 CREATE TABLE medicalPrescription (
  prescription_code serial PRIMARY KEY, 
- civ VARCHAR(16) NOT NULL,
+ cf VARCHAR(16) NOT NULL,
  telephone_number VARCHAR(10),
  priority VARCHAR(1) CHECK (priority = 'P'
 								OR priority = 'D'
 								OR priority = 'U'),
- medic_CIV VARCHAR(16) NOT NULL,
+ medic_cf VARCHAR(16) NOT NULL,
  description VARCHAR(512),
  medical_service_code VARCHAR(6),
- date_of_creation DATE,
+ date_of_creation DATE, 
  date_of_insertion TIMESTAMP
 )
